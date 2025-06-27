fc8bc7fa0c4692c7617a7e813f4ba491acce001f
GD <ganeshdeepak65@gmail.com>
2025-06-27 11:45:11 +0530

    add files

---
+++ android/app/src/main/kotlin/com/example/flayer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flayer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: 150,
+            height: 150,
+            color: Color(0xFFD9D9D9),
+          )
+        ],
+      ),
+    );
+  }
+}
